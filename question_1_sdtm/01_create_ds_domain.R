4860caaf3d3487857f80d47d2e4ee1a19336d452
escial <elsa.scialom@epfl.ch>
2026-02-25 12:41:43 +0000

    Added temporary df to enable direct mapping of DSTERM, DSDECOD, DSCAT

--- question_1_sdtm/01_create_ds_domain.R
+++ question_1_sdtm/01_create_ds_domain.R
@@ -6,8 +6,39 @@ library(dplyr)
 ds.raw <- pharmaverseraw::ds_raw
 study.ct <- read.csv("question_1_sdtm/sdtm_ct.csv")
 
+# Prep the mapping of variables
+# Create temporary df to enable direct mapping of DSTERM, DSDECOD and DSCAT
+ds.temp <- ds.raw
+ds.temp <- ds.temp %>%
+  # Creates a temp DSDECOD variable with the value of IT.DSDECOD if OTHERSP is
+  # null, and with the value of OTHERSP if any
+  mutate(
+    TEMP.DSDECOD = case_when(
+      is.na(OTHERSP) ~ IT.DSDECOD,
+      TRUE ~ OTHERSP
+    )
+  ) %>%
+  # Creates a temp DSTERM variable which is equal to value in OTHERSP if provided
+  # TODO: Maybe we don't need that and we map directly
+  mutate(
+    TEMP.DSTERM = case_when(
+      is.na(OTHERSP) ~ IT.DSTERM,
+      TRUE ~ OTHERSP
+    )
+  ) %>%
+  # Creates a temp DSCAT variable to be "PROTOCOL MILESTONE", "OTHER EVENT" or
+  # "DISPOSITION EVENT"
+  mutate(
+    TEMP.DSCAT = case_when(
+      IT.DSDECOD == "Randomized" & is.na(OTHERSP)  ~ "PROTOCOL MILESTONE",
+      !is.na(OTHERSP) ~ "OTHER EVENT",
+      TRUE ~ "DISPOSITION EVENT"
+    )
+  )
+
+
 # Create oak IDs to enable topic variable mapping
-ds.raw <- ds.raw %>%
+ds.temp <- ds.temp %>%
   generate_oak_id_vars(
     pat_var = "PATNUM",
     raw_src = "ds.raw"
@@ -18,24 +49,12 @@ ds.raw <- ds.raw %>%
 # No ct because this is the verbatim
 ds <-
   assign_no_ct(
-    raw_dat = ds.raw,
-    raw_var = "IT.DSTERM",
+    raw_dat = ds.temp,
+    raw_var = "TEMP.DSTERM",
     tgt_var = "DSTERM",
     id_vars = oak_id_vars()
   )
 
-# Prep the mapping of other variables
-# TODO: Later, write a function to extract the codes
-# Create temporary df to enable direct mapping
-ds.temp <- ds.raw
-# Create a temporary DSCAT variable which should be later mapped in ds
-ds.temp <- ds.temp %>%
-  mutate(
-    TEMP.DSCAT = case_when(
-      IT.DSDECOD == "Randomized" ~ "PROTOCOL MILESTONE",
-      TRUE ~ "DISPOSITION EVENT"
-    )
-  )
 
 # Map other variables
 ds <- ds %>% 
@@ -47,16 +66,16 @@ ds <- ds %>%
     raw_fmt = c("m-d-y"),
     id_vars = oak_id_vars()
   ) %>%
-  # Map the value of "IT.DSDECOD" in "DSDECOD" when OTHERSP is null
+  # Map the value of "TEMP.DSDECOD" in "DSDECOD"
   assign_ct(
-    raw_dat = ds.raw,
-    raw_var = "IT.DSDECOD",
+    raw_dat = ds.temp,
+    raw_var = "TEMP.DSDECOD",
     tgt_var = "DSDECOD",
     ct_spec = study.ct,
     ct_clst = "C66727",
     id_vars = oak_id_vars()
   ) %>%
-  # Map the values in "DSCAT"
+  # Map the values if "TEMP.DSCAT" in "DSCAT"
   assign_ct(
     raw_dat = ds.temp,
     raw_var = "TEMP.DSCAT",
