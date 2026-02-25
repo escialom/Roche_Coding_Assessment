1789f4ae54b6ece328ae824621c4ab517d257a27
escial <elsa.scialom@epfl.ch>
2026-02-25 10:57:19 +0000

    Mapped DSSTDTC, DSDECOD and DSCAT

--- question_1_sdtm/01_create_ds_domain.R
+++ question_1_sdtm/01_create_ds_domain.R
@@ -24,20 +24,54 @@ ds <-
     id_vars = oak_id_vars()
   )
 
+# Prep the mapping of other variables
+# TODO: Later, write a function to extract the codes
+# Create temporary df to enable direct mapping
+ds.temp <- ds.raw
+# Create a temporary DSCAT variable which should be later mapped in ds
+ds.temp <- ds.temp %>%
+  mutate(
+    TEMP.DSCAT = case_when(
+      IT.DSDECOD == "Randomized" ~ "PROTOCOL MILESTONE",
+      TRUE ~ "DISPOSITION EVENT"
+    )
+  )
 
-
-
-
-
-# Map the other variables
+# Map other variables
 ds <- ds %>% 
+  # Map date in ISO 8601 format
   assign_datetime(
     raw_dat = ds.raw,
     raw_var = "IT.DSSTDAT",
     tgt_var = "DSSTDTC",
     raw_fmt = c("m-d-y"),
     id_vars = oak_id_vars()
-)
+  ) %>%
+  # Map the value of "IT.DSDECOD" in "DSDECOD" when OTHERSP is null
+  assign_ct(
+    raw_dat = ds.raw,
+    raw_var = "IT.DSDECOD",
+    tgt_var = "DSDECOD",
+    ct_spec = study.ct,
+    ct_clst = "C66727",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map the values in "DSCAT"
+  assign_ct(
+    raw_dat = ds.temp,
+    raw_var = "TEMP.DSCAT",
+    tgt_var = "DSCAT",
+    ct_spec = study.ct,
+    ct_clst = "C74558",
+    id_vars = oak_id_vars()
+  )
+
+
+
+
+
+
+
 
 
 # Map the variables involving controlled terminology
