1e3f3eb270dfdcdfbcbe9ed6a355042d0c5f7e34
escial <elsa.scialom@epfl.ch>
2026-02-26 11:21:47 +0000

    Derived and added variable ITTFL

--- question_2_adam/create_adsl.R
+++ question_2_adam/create_adsl.R
@@ -25,7 +25,7 @@ vs <- convert_blanks_to_na(vs)
 adsl <- dm %>%
   select(-DOMAIN)
 
-# Derive AGEGR9 & AGEGR9N
+#### Derive AGEGR9 & AGEGR9N ####
 # we use the exprs function from tidyr to build the definition later used by
 # derive_vars_cat.
 agegr9.lookup <- exprs(
@@ -35,14 +35,31 @@ agegr9.lookup <- exprs(
   between(AGE, 18, 50),  "18-50",        2,
   !is.na(AGE),             ">50",        3
 )
+
 # Create variables AGEGR9 & AGEGR9N from ADSL according to the conditions set in
 # agegr9.lookup
-adsl.cat <- derive_vars_cat(
+adsl <- derive_vars_cat(
   dataset = adsl,
   definition = agegr9.lookup
 )
 
 
+#### Derive ITTFL ####
+# To know whether the subject has been randomized, we first have to check if
+# there are any missing values and/or what are the type of values in ARM
+any(is.na(adsl$ARM)) # No missing values
+ARM.groups <- unique(adsl$ARM) 
+
+# Set the ITTFL variable as "Y", except when screening failed. In this case, we
+# assume that the subject is not participating in the study and was not randomized
+adsl <- adsl %>%
+  dplyr::mutate(
+    ITTFL = case_when(
+      # ARM.groups[4] = "Screen Failure" 
+      ARM == ARM.groups[4] ~ "N",
+      TRUE ~ "Y"
+    )
+  )
 
 
 
