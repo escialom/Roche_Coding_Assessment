62c7270a87b62fe0171821f249a77a39ef41e228
escial <elsa.scialom@epfl.ch>
2026-02-26 13:31:21 +0000

    Derived TRTSDTM and TRTSTMF

--- question_2_adam/create_adsl.R
+++ question_2_adam/create_adsl.R
@@ -62,4 +62,46 @@ adsl <- adsl %>%
   )
 
 
+#### Derive TRTSDTM/TRTSTMF ####
+# Get the subset of EX dataset having valid casesDetect the valid dates
+ex.valid <- ex %>%
+  filter(
+    # Valid dosis if EXDOSE > 0 or if EXDOSE == 0 and EXTRT is "PLACEBO"
+    EXDOSE > 0 | (EXDOSE == 0 & stringr::str_detect(toupper(EXTRT), "PLACEBO"))
+    ) %>%
+  # Valid date if YYYY-MM-DD
+  filter(
+    stringr::str_detect(EXSTDTC, "^\\d{4}-\\d{2}-\\d{2}")
+    ) %>%
+  # From this valid subset, impute time if needed
+  derive_vars_dtm(
+    new_vars_prefix = "TRTS",
+    dtc = EXSTDTC,
+    # We do not allow imputation higher than the level of hour
+    highest_imputation = "h",   
+    # Imputes the earliest missing time
+    time_imputation = "first",
+    # For the TRTSTMF variable
+    flag_imputation = "time",
+    # No flag in TRTSTMF if seconds are missing
+    ignore_seconds_flag = TRUE
+  )
+
+# Merge back into adsl
+adsl <- adsl %>%
+  derive_vars_merged(
+    dataset_add = ex.valid,
+    # Use subject variables to merge back to adsl
+    by_vars = exprs(STUDYID, USUBJID),
+    new_vars = exprs(
+      TRTSDTM = TRTSDTM,
+      TRTSTMF = TRTSTMF
+    ),
+    # We use 2 expressions because it prevents ties in the case a subject has more
+    # than 1 exposure record
+    order = exprs(TRTSDTM, EXSEQ),
+    # Takes the first observation
+    mode = "first"
+  )
+
 
