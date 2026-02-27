693ec6f7f27685aa49045fcccbf71fb5fda4b7db
escial <elsa.scialom@epfl.ch>
2026-02-27 10:30:23 +0000

    Get top 10 most frequent AE and their respective percentage and CIs

--- question_3_tlg/02_create_visualizations.R
+++ question_3_tlg/02_create_visualizations.R
@@ -1,12 +1,15 @@
 library(ggplot2)
 library(pharmaverseadam)
+library(purrr)
 
 
 # Load ADaM datasets from {pharmaverseadam} 
 adae <- pharmaverseadam::adae
 adsl <- pharmaverseadam::adsl
 
-# Get plot 1
+
+#### Get plot 1 ####
+
 plt1 <- adae %>%
   filter(TRTEMFL == "Y") %>%
   count(ACTARM, AESEV) %>%
@@ -22,3 +25,31 @@ plt1 <- adae %>%
 
 # Save plot 1
 ggsave("question_3_tlg/plot1.png", plot = plt1)
+
+
+#### Get plot 2 ####
+
+# Get the total number of subjects to calculate percentage
+N.subj <- length(unique(adae$USUBJID))
+
+# Get the percentages of AE and their confidence interval
+ae.stats <- adae %>%
+  distinct(USUBJID, AEDECOD) %>%   
+  group_by(AEDECOD) %>%
+  summarize(
+    n = n(),                      
+    percent = n / N.subj * 100,
+    # For each AE term, run an exact binomial test:
+    # Given n events out of N subjects, what is the exact 95% confidence interval for the true probability p?
+    # We use the mapping to apply the binomial test to each n
+    # .x is the syntax to use to denote the current n
+    bt = map(n, ~binom.test(.x, N.subj, conf.level = 0.95)),
+    # Extract the lower CI bound from each binom.test result and convert to %
+    # Use map_dbl to ensure numeric output
+    lower = map_dbl(bt, ~.x$conf.int[1] * 100),
+    # Extract the upper CI bound from each binom.test result and convert to %
+    upper = map_dbl(bt, ~.x$conf.int[2] * 100)
+  )%>%
+  arrange(desc(percent)) %>%
+  slice_head(n = 10)
+
